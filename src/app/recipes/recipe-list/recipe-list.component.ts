279864585fab1a07cf5811b01bfabb1976ef6f87
sylwuskak <sylwuskak@gmail.com>
2018-10-02 16:12:04 +0200

    navigation

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,7 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('test recipe', 'this is simply a test', 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2017%2F06%2Fmain%2Frigatoni-kale-pesto-1708p12.jpg%3Fitok%3DZRmUV5oK&w=700&q=85')
     
@@ -15,6 +16,11 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onRecipeSelected(recipe: Recipe) {
+    console.log("list " + recipe)
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
