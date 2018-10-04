16f77443780c4a52a581434bfc97344fcef8314f
sylwuskak <sylwuskak@gmail.com>
2018-10-04 09:33:41 +0200

    directives

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, HostListener, ElementRef, Renderer2, HostBinding } from "@angular/core";
+
+@Directive({
+    selector: '[appDropdown]'
+})
+export class DropdownDirective{
+    @HostBinding('class.open') isOpen = false;
+    
+    @HostListener('click') clickMethod(evenData: Event) {
+        this.isOpen = !this.isOpen;
+    }
+
+
+}
